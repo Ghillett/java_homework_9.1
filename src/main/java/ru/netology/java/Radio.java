e48e5a7d0db8dba35775a67a6725e30929490c5e
JP <email>
2022-11-14 21:47:26 +1000

    solution

---
+++ src/main/java/ru/netology/java/Radio.java
@@ -0,0 +1,54 @@
+package ru.netology.java;
+
+public class Radio {
+
+    private int stationNumber = 0;
+    private int volume = 0;
+
+
+    public int getStationNumber(){
+        return stationNumber;
+    }
+
+    public int getVolume(){
+        return volume;
+    }
+
+    public void setStation(int stationNumber){
+        if(stationNumber >= 0){
+            if(stationNumber <= 9){
+                this.stationNumber = stationNumber;
+            }
+        }
+    }
+    public void next(){
+        if(stationNumber < 9){
+            stationNumber++;
+        }
+        else{
+            stationNumber = 0;
+        }
+    }
+
+    public void prev(){
+        if(stationNumber > 0){
+            stationNumber--;
+        }
+        else{
+            stationNumber = 9;
+        }
+    }
+
+    public void increaseVolume(){
+
+        if(volume < 10) {
+            volume++;
+        }
+    }
+
+    public void decreaseVolume(){
+        if(volume > 0){
+            volume--;
+        }
+    }
+}
---
+++ src/test/java/ru/netology/java/RadioTest.java
@@ -0,0 +1,125 @@
+package ru.netology.java;
+
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class RadioTest {
+
+    //использовала для проверки метод граничных значений
+
+    @ParameterizedTest
+    @CsvSource({
+            "8, 8",
+            "9, 9",
+            "10, 0"
+    })
+    public void shouldCheckMaxStationSet(int stationNumber, int expected){
+
+        Radio radio = new Radio();
+
+        radio.setStation(stationNumber);
+
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "0, 0",
+            "-1, 0"
+    })
+    public void shouldCheckMinStationSet(int stationNumber, int expected){
+
+        Radio radio = new Radio();
+        radio.setStation(stationNumber);
+
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "7, 8",
+            "8, 9",
+            "9, 0"
+    })
+    public void shouldCheckMaxStationSwitch(int stationNumber, int expected){
+
+        Radio radio = new Radio();
+
+        radio.setStation(stationNumber);
+
+        radio.next();
+
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "2, 1",
+            "1, 0",
+            "0, 9"
+    })
+    public void shouldCheckMinStationSwitch(int stationNumber, int expected){
+
+        Radio radio = new Radio();
+
+        radio.setStation(stationNumber);
+
+        radio.prev();
+
+        int actual = radio.getStationNumber();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "9, 9",
+            "10, 10",
+            "11, 10"
+    })
+    public void shouldCheckMaxVolume(int clicks, int expected){
+
+        Radio radio = new Radio();
+        for(int i = 0; i < clicks; i++){
+            radio.increaseVolume();
+        }
+
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "1, 1",
+            "2, 0",
+            "3, 0"
+    })
+    public void shouldCheckMinVolume(int clicks, int expected){
+
+        Radio radio = new Radio();
+
+        //поднять громкость до 2, чтобы протестировать переход вниз на громкость 1
+
+        radio.increaseVolume();
+        radio.increaseVolume();
+
+        for (int i = 0; i < clicks; i++) {
+            radio.decreaseVolume();
+        }
+
+        int actual = radio.getVolume();
+
+        Assertions.assertEquals(expected, actual);
+    }
+}
